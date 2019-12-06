299d5e325225e755093c2cdad53c575f2c0ea950
Neunis <n_nunis@u.pacific.edu>
2019-12-06 00:59:20 -0800

    Git LFS initiated and Intro Mobile App Design File added

---
+++ HelloNaomi/app/src/main/java/com/example/neunis/hellonaomi/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.neunis.hellonaomi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView)findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimary));
+                }
+        });
+    }
+
+}
